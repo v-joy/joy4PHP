a05f1a104eb6e28277bce6aa24b82b54f52be415
joy <fulljoy.0@gmail.com>
2013-03-14 17:46:45 +0800

    add file

--- joy4PHP/Lib/View.class.php
+++ joy4PHP/Lib/View.class.php
@@ -1,4 +1,19 @@
 <?php
 class View{
 	
+	//store view variables
+	protected $vars = array();
+	
+	//assign view variable
+	public function assign($name,$value){
+		
+	}
+	
+	public function fetch($name){
+		
+	}
+	
+	public function display(){
+		
+	}
 }
---
+++ joy4PHP/joy4PHP.php
@@ -0,0 +1,10 @@
+<?php
+class joy4PHP{
+	public function __construct(){
+	
+	}
+	
+	public function run(){
+		
+	}
+}
