82f620e702670e54d7dc54f9fde6fc8fbab3b3e0
joy <fulljoyu.0@gmail.com>
2013-08-21 12:22:55 +0800

    change Controller file name

--- joy4PHP/joy4PHP.php
+++ joy4PHP/joy4PHP.php
@@ -48,7 +48,7 @@ class joy4PHP{
 		//use reflaction to call requested method in class
 		$module = Dispatcher::getModule();
 		$action = Dispatcher::getAction();
-		$controllerName = WEB_ROOT."/Controllers/".$module.".class.php";
+		$controllerName = WEB_ROOT."/Controllers/".$module."Controller.class.php";
 		if (is_file($controllerName)) {
 			require_once $controllerName;
 		}else if (is_file(WEB_ROOT."/Controllers/__empty.class.php")) {
